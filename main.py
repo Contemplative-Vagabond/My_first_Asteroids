06ab16a1a258f1777c283f2b5167a7988e30ad9a
Contemplative-Vagabond <dg.contemplativevagabond@gmail.com>
2025-03-12 19:43:53 +0100

    add asteroids and make them spawn at random

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import*
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     # Initialize pygame modules
@@ -15,8 +16,12 @@ def main():
     # Initialize game objeccts
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     # Game Loop:
     running = True
