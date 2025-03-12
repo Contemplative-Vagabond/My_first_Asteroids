f610cd5f41a73a2a691bff7452fad25f06540ed8
Contemplative-Vagabond <dg.contemplativevagabond@gmail.com>
2025-03-12 18:31:05 +0100

    add circleshape and player class files, update main and constants to render player

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import pygame
 from constants import *
+from circleshape import*
+from player import*
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+    # Initialize pygame modules
     pygame.init()
+
+    # Set game logic parameters
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    # Game Loop:
+    running = True
+    while running:
+        # Handle events
         for event in pygame.event.get():
+            # Enable quit button
             if event.type == pygame.QUIT:
-                return
-        screen.fill((0, 0 ,0))
+                running = False
+        
+        # Draw game objects
+        screen.fill((0, 0 ,0)) # Black background
+        player.draw(screen)
+        
+        # Update display
         pygame.display.flip()
+        
+        # Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
